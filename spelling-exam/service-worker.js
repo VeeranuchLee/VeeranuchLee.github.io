0517487ca77e7004328339f140104bfe67215ac7
VeeranuchLee <magelet8@gmail.com>
2026-08-30 15:54:22 +0700

    Use new word artwork in Spelling Exam

--- spelling-exam/service-worker.js
+++ spelling-exam/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='spelling-exam-v1';
+const CACHE='spelling-exam-v2';
 const SHELL=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k.startsWith('spelling-exam-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
